42eb534f8d08c677cbdcb164536d2fcee9fba957
Nightvision65 <1615858537@qq.com>
2022-04-04 16:33:21 +0800

    假面骑士变身！剑形态！ 以及三代

---
+++ Assets/BitFish_LD/Script/65_Script/Enemy_Body_Destroyer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Body_Destroyer : MonoBehaviour
+{
+    public float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
