795b97a41ca2b6b506a6feb34ed647eee6c6ed4d
oo2222oo <alfredapple@qq.com>
2022-04-05 22:04:13 +0800

    A?

--- Assets/BitFish_LD/Obj_Save/Weapon_Save/Weapon_Data.cs
+++ Assets/BitFish_LD/Obj_Save/Weapon_Save/Weapon_Data.cs
@@ -5,8 +5,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewWeapon", menuName = "Weapon/NewWeapon")]
 public class Weapon_Data : Item_Get_Data
 {
-    public Sprite Weapon_Icon;
-    public float Weapon_Damge;
-    public float Weapon_Bullet_Max;
-    public float Weapon_KnockBack;
+    public Weapon_Manager Weapon_manager;
 }
